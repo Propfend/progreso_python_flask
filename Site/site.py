2d3f78e2019691684558c47f71bea75dc5261c5c
Propfend <120745901+Propfend@users.noreply.github.com>
2023-09-11 13:19:13 -0300

    commit

--- Site/site.py
+++ Site/site.py
@@ -1,14 +1,11 @@
 from flask import Flask
 from flask import render_template
+site = Flask(__name__)
 
-
-site = Flask('__name__')
-
-
-@site.route('/ola')
-def ola():
-    return render_template('index.html')
-
+@site.route('/', defaults={'nome':None})
+@site.route('/ola/<nome>')
+def ola(nome):
+    return render_template('index.html', nome=nome)
 
 if __name__ == '__main__':
     site.run(debug=True, port=3000)
